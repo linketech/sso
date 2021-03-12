5432e13642ae22502590a3a503f29a6bb265d736
XueHua Huang <xuehua.huang@dbjtech.com>
2021-03-12 13:54:56 +0800

    修复user没有role时报错的问题

--- app/controller/user.js
+++ app/controller/user.js
@@ -11,7 +11,7 @@ module.exports = class UserController extends Controller {
 			id: id.toString('hex'),
 			name,
 			disabled,
-			role_id: role_id.toString('hex'),
+			role_id: role_id && role_id.toString('hex'),
 			role_name,
 		}))
 	}
